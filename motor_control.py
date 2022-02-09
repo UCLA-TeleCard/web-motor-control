b578812c0d9e143546797398728f8144a2490312
leadweedy <dicorned@gmail.com>
2022-02-08 16:01:08 -0800

    temporarily remove Pi specific code

--- motor_control.py
+++ motor_control.py
@@ -3,37 +3,37 @@ from flask import request
 
 import time
 import atexit
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 from time import sleep
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(11, GPIO.OUT)
 
-pwm=GPIO.PWM(11, 50)
-pwm.start(0)
+# pwm=GPIO.PWM(11, 50)
+# pwm.start(0)
 
-def turnOffMotors():
-  pwm.stop()
-  GPIO.cleanup()
+# def turnOffMotors():
+#   pwm.stop()
+#   GPIO.cleanup()
 
-def setAngle(angle):
-    duty = angle / 18 + 3
-    GPIO.output(11, True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(11, False)
-    pwm.ChangeDutyCycle(duty)
+# def setAngle(angle):
+#     duty = angle / 18 + 3
+#     GPIO.output(11, True)
+#     pwm.ChangeDutyCycle(duty)
+#     sleep(1)
+#     GPIO.output(11, False)
+#     pwm.ChangeDutyCycle(duty)
 
-atexit.register(turnOffMotors)
+# atexit.register(turnOffMotors)
 
 @app.route("/")
 def web_interface():
   html = open("web_interface.html")
   response = html.read().replace('\n', '')
   html.close()
-  pwm.ChangeDutyCycle(7.5) # neutral position
+  #pwm.ChangeDutyCycle(7.5) # neutral position
   sleep(1)
   return response
 
@@ -42,10 +42,34 @@ def set_speed():
   speed = int(request.args.get("speed"))
   print ("Received " + str(speed))
 
-  pwm.ChangeDutyCycle(speed)
+  #pwm.ChangeDutyCycle(speed)
   sleep(1)
 
   return ("Received " + str(speed))
+
+@app.route("/set_toggle")
+def set_toggle():
+  state = request.args.get("state")
+  print ("Received " + str(state))
+
+  # pwm.ChangeDutyCycle(state)
+  sleep(1)
+
+  return ("Received " + str(state))
+
+
+@app.route("/set_button")
+def set_button():
+  butt = request.args.get("butt")
+  print ("Received " + str(butt))
+
+  # pwm.ChangeDutyCycle(state)
+  sleep(1)
+
+  return ("Received " + str(butt))
+
+
+
 
 def main():
   app.run(host= '0.0.0.0')
